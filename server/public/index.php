f026492cc58929291ad7289a0105e75b00528c9d
Teslima02 <teslimafeez@gmail.com>
2018-12-22 20:31:56 +0100

    update

--- server/public/index.php
+++ server/public/index.php
@@ -22,7 +22,7 @@ define('LARAVEL_START', microtime(true));
 */
 
  
-require __DIR__.'/../datasecur/server/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +36,7 @@ require __DIR__.'/../datasecur/server/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../datasecur/server/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
